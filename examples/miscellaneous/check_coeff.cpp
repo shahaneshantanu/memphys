e17175e421268adfdf522923c5b17904e32786dc
shahaneshantanu <shahaneshantanu@gmail.com>
2021-05-29 22:48:02 -0500

    Miscellaneous codes updated

--- examples/miscellaneous/check_coeff.cpp
+++ examples/miscellaneous/check_coeff.cpp
@@ -11,8 +11,8 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
     clock_t t0 = clock();
 
-    // string meshfile = "/home/shantanu/Desktop/All Simulation Results/Meshless_Methods/CAD_mesh_files/Square/gmsh/Square_n_10_unstruc.msh"; //2D example
-    string meshfile = "/home/shantanu/Desktop/All Simulation Results/Meshless_Methods/CAD_mesh_files/cuboid/Cuboid_n_10_unstruc.msh"; //3D example
+    string meshfile = "/media/shantanu/Data/All Simulation Results/Meshless_Methods/CAD_mesh_files/Square/gmsh/Square_n_10_unstruc.msh"; //2D example
+    // string meshfile = "/media/shantanu/Data/All Simulation Results/Meshless_Methods/CAD_mesh_files/cuboid/Cuboid_n_10_unstruc.msh"; //3D example
 
     PARAMETERS parameters("parameters_file.csv", meshfile);
     POINTS points(parameters);
